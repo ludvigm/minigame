b026dba8667b07db2359024ce6a561de0939820a
lm222ix <lm222ix@student.lnu.se>
2016-03-09 19:12:50 +0100

    .

---
+++ minigame/Assets/backgroundScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ResizeSpriteToScreen();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ResizeSpriteToScreen() {
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		if (sr == null) return;
+
+		transform.localScale = new Vector3(1,1,1);
+
+		float width = sr.sprite.bounds.size.x;
+		float height = sr.sprite.bounds.size.y;
+
+		float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+		transform.localScale.Set (worldScreenWidth/width, worldScreenHeight / height, 1);
+	}
+}
---
+++ minigame/Assets/playerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+	public float speed;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			
+			Vector3 mousePos = Input.mousePosition;
+			mousePos.z = 10;
+			Vector3 modifiedPos = Camera.main.ScreenToWorldPoint (mousePos);
+
+			if (mouseIsLeftOfPlayer (modifiedPos)) {
+				rb.velocity = new Vector2 (speed, 0);
+			} else {
+				rb.velocity = new Vector2 (-speed, 0);
+			}
+		}
+	}
+
+	private bool mouseIsLeftOfPlayer(Vector3 mousePos) {
+		if (rb.position.x < mousePos.x) {
+			return true;
+		}
+		return false;
+	}
+}
